1c2f0deafdb5a9a262ebcb07981d7787f9d03e7c
Rahul16-creator <rahulkannanrk1999@gmail.com>
2021-09-27 19:56:26 +0530

    change in test case

--- src/test/java/com/example/redi_swagger/RediSwaggerApplicationTests.java
+++ src/test/java/com/example/redi_swagger/RediSwaggerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.redi_swagger;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RediSwaggerApplicationTests {
 
+    static final Logger log = LoggerFactory.getLogger(RediSwaggerApplication.class);
+
+
     @Test
     void contextLoads() {
+        log.info("Test cases executed successfully!!");
     }
+
 
 }

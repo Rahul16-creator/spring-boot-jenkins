77da84363b5481f4ea25a78c0c4f1d52131c2054
Rahul16-creator <rahulkannanrk1999@gmail.com>
2021-09-27 18:09:22 +0530

    changes

--- src/main/java/com/example/redi_swagger/RediSwaggerApplication.java
+++ src/main/java/com/example/redi_swagger/RediSwaggerApplication.java
@@ -1,12 +1,17 @@
 package com.example.redi_swagger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class RediSwaggerApplication {
+    static final Logger log = LoggerFactory.getLogger(RediSwaggerApplication.class);
 
     public static void main(String[] args) {
+        log.info("All done successfully!!");
         SpringApplication.run(RediSwaggerApplication.class, args);
     }
 
